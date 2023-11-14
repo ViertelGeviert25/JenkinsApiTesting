c54619d774ea5e2b42f90aed9d87154d7a308af4
BlaueTulpe <phi.weber@proton.me>
2023-11-14 19:55:04 +0100

    Jenkinsfile new stage

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,6 +8,16 @@ pipeline {
                     bat '"H:\\dev\\JenkinsApiTesting\\DoSomething\\bin\\Debug\\DoSomething.exe"'
                 }
         }
+        stage('Sync') [
+            steps {
+                def get = new URL("https://httpbin.org/get").openConnection();
+                def getRC = get.getResponseCode();
+                println(getRC);
+                if(getRC.equals(200)) {
+                    println(get.getInputStream().getText());
+                }
+            }
+        ]
     }
 }
 }
